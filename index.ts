01c3929d23de32f399b278d3af3ae4fe6ba37d57
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-22 01:52:13 +0530

    Build the Graph

--- index.ts
+++ index.ts
@@ -1,5 +1,29 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+import { MessagesAnnotation, StateGraph } from '@langchain/langgraph';
+import { HumanMessage } from 'langchain';
+
+/*
+* 1. Define Node Functions.
+* 2. Build the Graph.
+* 3. Compile and Invoke the Graph.
+*/
+
+function callModal(state: any) {
+    // Call the LLM using APIs
+    console.log("Calling LLM...");
+
+    return state;
+}
+
+/** Build the Graph! **/
+const workflow = new StateGraph(MessagesAnnotation)
+    .addNode("agent", callModal)
+    .addEdge("__start__", "agent")
+    .addEdge("agent", "__end__");
+
+/** Compile the Graph! **/
+const app = workflow.compile();
 
 async function main() {
     const rl = readline.createInterface({ input, output });
@@ -8,7 +32,12 @@ async function main() {
         const userInput = await rl.question("You: ");
         if (userInput === "/bye") break;
 
-        console.log(`You said: ${userInput}`);
+        /** Invoke the App! **/
+        const finalState = await app.invoke({
+            messages: [{ role: "user", content: userInput }]
+        });
+
+        console.log(`Final: ${finalState}`);
     }
 
     rl.close();
